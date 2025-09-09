d23d87b95281f8f5837c1c20a99902bdea45b745
sara <sarajaved0789@gmail.com>
2025-09-09 21:41:01 +0500

    First part is done

---
+++ pandas_intro.py
@@ -0,0 +1,13 @@
+import pandas as pd
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Hennery",
+            "Bonnell, Miss. Elizebeth"
+        ],
+        "Age": [22,35,58],
+        "Sex": ["male","male","female"],
+    }
+)
+df
